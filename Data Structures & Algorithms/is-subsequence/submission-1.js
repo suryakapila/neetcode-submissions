f981601745cc11264917f5b1ff61d33420bff004
Surya Kapila <34037638+suryakapila@users.noreply.github.com>
2026-05-07 14:40:51 +0530

    Add: is-subsequence - submission-1

---
+++ Data Structures & Algorithms/is-subsequence/submission-1.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isSubsequence(s, t) {
+        let i = 0, j = 0; 
+        while(i < s.length && j < t.length){
+            if(s[i] == t[j]){
+                i++;
+            }
+            j++;
+        }
+        if(i==s.length) return true;
+        return false;
+    }
+}
